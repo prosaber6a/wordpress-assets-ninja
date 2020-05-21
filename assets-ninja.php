0066059d855d5b792fff35440455e8aca9fcffef
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-05-21 16:06:07 +0600

    Plugin Head

---
+++ assets-ninja.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       Assets Ninja
+ * Plugin URI:        https://saberhr.com/
+ * Description:       WordPress Assets Management in Depth
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            SaberHR
+ * Author URI:        https://saberhr.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       assets-ninja
+ * Domain Path:       /languages
+ */
