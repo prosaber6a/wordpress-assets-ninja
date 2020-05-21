0f9fd79aad979b0ceca7c8464ba8c901926d1abb
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-05-21 16:33:15 +0600

    Assets Dependency

--- assets-ninja.php
+++ assets-ninja.php
@@ -13,3 +13,31 @@
  * Text Domain:       assets-ninja
  * Domain Path:       /languages
  */
+
+define( "ASN_ASSETS_DIR", plugin_dir_url( __FILE__ ) . "/assets/" );
+define( "ASN_ASSETS_PUBLIC_DIR", plugin_dir_url( __FILE__ ) . "/assets/public/" );
+define( "ASN_ASSETS_ADMIN_DIR", plugin_dir_url( __FILE__ ) . "/assets/admin/" );
+
+class AssetsNinja {
+	private $version;
+
+	public function __construct() {
+		$this->version = time();
+		add_action( 'plugins_loaded', array( $this, 'load_text_domain' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'load_front_assets' ) );
+	}
+
+	public function load_text_domain() {
+		load_plugin_textdomain( 'assets-ninja', false, plugin_dir_url( __FILE__ ) . "/languages" );
+	}
+
+	public function load_front_assets() {
+		wp_enqueue_script( 'asn-main-js', ASN_ASSETS_PUBLIC_DIR . "/js/main.js", array( 'jquery', 'asn-another-js' ), $this->version, true );
+		wp_enqueue_script( 'asn-another-js', ASN_ASSETS_PUBLIC_DIR . "/js/another.js", array( 'jquery', 'asn-more-js' ), $this->version, true );
+		wp_enqueue_script( 'asn-more-js', ASN_ASSETS_PUBLIC_DIR . "/js/more.js", array( 'jquery' ), $this->version, true );
+
+	}
+}
+
+
+new AssetsNinja();
